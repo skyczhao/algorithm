af86fa89962b62847aab184c01faae81f18c31af
Tobin <chenzhao.me@gmail.com>
2014-01-02 17:55:13 +0800

    strange algorithm

--- hua_test/main.cpp
+++ hua_test/main.cpp
@@ -1,33 +1,58 @@
 
 #include <iostream>
+#include <string.h>
+#include <math.h>
 using namespace std;
 
-int list[1000005];
+#define DIGITS 1000000000
+int matrix[33][1000005];
 int size;
 
-void split(int num, int next)
+int subfunc(int num, int level)
 {
-    int i, old_size;
-    if(num == 0){
-        size++;
-        return;
+    if(level == 1){
+        matrix[0][num] = 1;
+        return 1;
     }
 
-    for(i = 1; i <= num; i *= 2){
-        if(next == 0 || i <= list[next - 1]){
-            list[next] = i;
-            split(num - i, next + 1);
+    int t_level = log((double)level) / log((double)2);
+    if(matrix[t_level][num] != 0)
+        return matrix[t_level][num];
+
+    int t_sum = 0;
+    int t_size = num / level;
+    for(int t_i = 0; t_i <= t_size; t_i++){
+        int t_num = num - t_i * level;
+        if(matrix[t_level - 1][t_num] == 0){
+            matrix[t_level - 1][t_num] = subfunc(t_num, level / 2);
+            if(t_num % 2 == 0)
+                matrix[t_level - 1][t_num + 1] = matrix[t_level - 1][t_num];
+            else
+                matrix[t_level - 1][t_num - 1] = matrix[t_level - 1][t_num];
         }
+        t_sum += matrix[t_level - 1][t_num];
+        t_sum %= DIGITS;
     }
+
+    return t_sum;
 }
 
 int main()
 {
-    int num;
+    int num, i;
+    memset(matrix, 0, sizeof(matrix));
+
     while(cin >> num){
         size = 0;
-        split(num, 0);
-        cout << size << endl;
+        for(i = 1; i <= num; i *= 2){
+            if(i == 1)
+                size++;
+            else{
+                size = size + subfunc(num - i, i);
+                size %= DIGITS;
+            }
+        }
+        cout << num << ":" << size << endl;
     }
     return 0;
 }
