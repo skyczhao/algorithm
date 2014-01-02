71aaa6ae920838c13ada3ddc050119b529e9b47e
Tobin <chenzhao.me@gmail.com>
2014-01-02 18:07:50 +0800

    change a little

--- hua_test/main.cpp
+++ hua_test/main.cpp
@@ -5,32 +5,28 @@
 using namespace std;
 
 #define DIGITS 1000000000
-int matrix[33][1000005];
+int matrix[33][500005];
 int size;
 
 int subfunc(int num, int level)
 {
     if(level == 1){
-        matrix[0][num] = 1;
+        matrix[0][num >> 1] = 1;
         return 1;
     }
 
     int t_level = log((double)level) / log((double)2);
-    if(matrix[t_level][num] != 0)
-        return matrix[t_level][num];
+    if(matrix[t_level][num >> 1] != 0)
+        return matrix[t_level][num >> 1];
 
     int t_sum = 0;
     int t_size = num / level;
     for(int t_i = 0; t_i <= t_size; t_i++){
         int t_num = num - t_i * level;
-        if(matrix[t_level - 1][t_num] == 0){
-            matrix[t_level - 1][t_num] = subfunc(t_num, level / 2);
-            if(t_num % 2 == 0)
-                matrix[t_level - 1][t_num + 1] = matrix[t_level - 1][t_num];
-            else
-                matrix[t_level - 1][t_num - 1] = matrix[t_level - 1][t_num];
+        if(matrix[t_level - 1][t_num >> 1] == 0){
+            matrix[t_level - 1][t_num >> 1] = subfunc(t_num, level / 2);
         }
-        t_sum += matrix[t_level - 1][t_num];
+        t_sum += matrix[t_level - 1][t_num >> 1];
         t_sum %= DIGITS;
     }
 
