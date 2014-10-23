ce1acab15bf928809e99a76ac4b983cb4ab270d8
Tobin <chenzhao.me@gmail.com>
2014-10-23 11:04:31 +0800

    integer 0 as bool false, and other integers(including positive and negative) as bool true

--- soj1198/main.cpp
+++ soj1198/main.cpp
@@ -1,34 +1,43 @@
 
 #include <stdio.h>
-#include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
-bool smaller(string x, string y)
+bool smaller(char *x, char *y)
 {
-    return (x + y < y + x);
+    char x_f[202], y_f[202];
+    strcpy(x_f, x);
+    strcat(x_f, y);
+    strcpy(y_f, y);
+    strcat(y_f, x);
+    if(strcmp(x_f, y_f) < 0)
+        return true;
+    else
+        return false;
 }
 
 int main()
 {
     int size, n, i, j;
-    string str[10];
-    string tmp;
+    char x_f[202], y_f[202];
+    char str[10][101];
+    char tmp[101];
 
     scanf("%d", &size);
     while(size--){
         scanf("%d", &n);
         for(i = 0; i < n; i++)
-            cin >> str[i];
+            scanf("%s", str[i]);
         for(i = 0; i < n; i++)
             for(j = i + 1; j < n; j++)
                 if(smaller(str[j], str[i])){
-                    tmp = str[j];
-                    str[j] = str[i];
-                    str[i] = tmp;
+                    strcpy(tmp, str[j]);
+                    strcpy(str[j], str[i]);
+                    strcpy(str[i], tmp);
                 }
+
         for(i = 0; i < n; i++)
-            cout << str[i];
+            printf("%s", str[i]);
         printf("\n");
     }
     return 0;
